eed73e807ce1e5b1bec099f65daea97e27ad68e1
jp2137 <sbaiyassine2004@gmail.com>
2022-10-18 09:17:50 +0200

    Initial commit + akcja 1

---
+++ app/src/main/java/com/example/pszysiski/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.pszysiski
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val label = findViewById<TextView>(R.id.textView)
+        val textbox = findViewById<TextView>(R.id.editTextTextPersonName)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            label.setText(label.text + textbox.text)
+        }
+    }
+}
